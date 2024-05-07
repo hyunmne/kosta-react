f381a44bc17b6761d3554580b032f6570d12c4e8
hyunmne <echo0913@naver.com>
2024-05-07 16:49:05 +0900

    react: css

--- bank1/src/App.js
+++ bank1/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { MakeAccount } from './MakeAccount';
+import AccountInfo from './AccountInfo';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import AllAccountInfo from './AllAccountInfo';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <MakeAccount/> */}
+    {/* <AccountInfo/> */}
+    {/* <Deposit/> */}
+      {/* <Withdraw/> */}
+      <AllAccountInfo/>
     </div>
   );
 }
---
+++ bank1/src/MakeAccount.js
@@ -0,0 +1,59 @@
+import {useState} from 'react';
+import './App.css';
+
+export const MakeAccount = () => {
+    
+    const [acc, setAcc] = useState({id:'',name:'',money:'',type:'normal',grade:''})
+    const changeValue = (e) => {
+        setAcc({...acc, [e.target.name]:e.target.value})
+    }
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(acc);
+    }
+
+    return (
+        <>
+            <h3>계좌 개설</h3>
+            <table border="1" >
+                <tbody>
+                    <tr>
+                        <th>계좌번호</th>
+                        <td><input type="text" name="id" onChange={changeValue} /></td>
+                    </tr>
+                    <tr>
+                        <th>이름</th>
+                        <td><input type="text" name="name" onChange={changeValue} /></td>
+                    </tr>
+                    <tr>
+                        <th>입금액</th>
+                        <td><input type="text" name="money" onChange={changeValue} /></td>
+                    </tr>
+                    <tr>
+                        <th>종류</th>
+                        <td>
+                            <input type="radio" name="type" value="normal"  onChange={changeValue} checked={acc.type=='normal'} />일반
+                            <input type="radio" name="type" value="special" onChange={changeValue} />특수
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>등급</th>
+                        <td>
+                            <select name="grade" onChange={changeValue} disabled={acc.type=='normal'}>
+                                <option value="">선택하세요</option>
+                                <option value="VIP">VIP</option>
+                                <option value="Gold">Gold</option>
+                                <option value="Silver">Silver</option>
+                                <option value="Normal">Normal</option>
+                            </select>    
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="개설" onClick={submit} /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
