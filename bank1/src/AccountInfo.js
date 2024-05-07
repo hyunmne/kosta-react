d2533887ff84663898a41ab1cf838cadc7781167
hyunmne <echo0913@naver.com>
2024-05-07 16:48:53 +0900

    react: 계좌조회

---
+++ bank1/src/AccountInfo.js
@@ -0,0 +1,56 @@
+import {useState} from 'react'; 
+import './App.css';
+
+export default function AccountInfo() {
+
+    const [accId, setAccId] = useState('');
+    const [acc, setAcc] = useState({id:'',name:'',money:'',type:'',grade:''})
+
+    const submit = (e) => {
+        e.preventDefault();
+        // 서버로부터 accId로 조회해 온 결과를 화면에 출력한다.
+        let racc = {id:accId, name:'홍길동',money:'100000',type:'special',grade:'VIP'}
+        setAcc({...racc})
+    }
+
+    return (
+        <div className="App">
+            <h3>계좌 조회</h3>
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <th>계좌번호</th>
+                        <td className="accValue">
+                            <input type="text" name="id" onChange={(e)=>setAccId(e.target.value)} />
+                            <input type="submit" value="조회" onClick={submit}/> 
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <th>계좌번호</th>
+                        <td className="accValue"><label>{acc.id}</label></td>
+                    </tr>
+                    <tr>
+                        <th>이름</th>
+                        <td className="accValue"><label>{acc.name}</label></td>
+                    </tr>
+                    <tr>
+                        <th>잔액</th>
+                        <td className="accValue"><label>{acc.money}</label></td>
+                    </tr>
+                    <tr>
+                        <th>종류</th>
+                        <td className="accValue"><label>{acc.type}</label></td>
+                    </tr>
+                    <tr>
+                        <th>등급</th>
+                        <td className="accValue"><label>{acc.grade}</label></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
