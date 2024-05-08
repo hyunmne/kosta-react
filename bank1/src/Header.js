aa9011cbb5d7841fceb66b98475097e2435c12c7
hyunmne <echo0913@naver.com>
2024-05-08 10:48:56 +0900

    bank1: header

--- bank1/src/Header.js
+++ bank1/src/Header.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 const Header = () => {
     const divStyle = {
         backgroundColor : '#ebe3b7',
@@ -12,11 +14,11 @@ const Header = () => {
         <div style={divStyle}>
             <ul className="nav-items">
                 <li className="nav-item"><i><b>코스타 은행</b></i></li>
-                <li className="nav-item">계좌개설</li>
-                <li className="nav-item">입금</li>
-                <li className="nav-item">출금</li>
-                <li className="nav-item">계좌조회</li>
-                <li className="nav-item">전체계좌조회</li>
+                <li className="nav-item"><Link to="/">계좌개설</Link></li>
+                <li className="nav-item"><Link to="/deposit">입금</Link></li>
+                <li className="nav-item"><Link to="/withdraw">출금</Link></li>
+                <li className="nav-item"><Link to="/accountInfo">계좌조회</Link></li>
+                <li className="nav-item"><Link to="/allAccountInfo">전체계좌조회</Link></li>
             </ul>
         </div>
     )
