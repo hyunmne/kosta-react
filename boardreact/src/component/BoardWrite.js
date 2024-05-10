0a8505962606542e6b98e80e977cff55b45a27b8
hyunmne <echo0913@naver.com>
2024-05-10 11:43:46 +0900

    react-board: boardWrite

--- boardreact/src/component/BoardWrite.js
+++ boardreact/src/component/BoardWrite.js
@@ -1,57 +1,82 @@
 import axios from 'axios';
-import {
-    Col, Button, FormGroup, Form, Label, Table, Input,
-    Pagination, PaginationItem, PaginationLink
-} from 'reactstrap';
+import { Input, Button, Label, Col, Form, FormGroup } from 'reactstrap';
 import { useState } from 'react';
+import {url} from '../config';
 
 const BoardWrite = () => {
-    // const formStyle = { width: "600px", textAlign: "center", margin: "0 auto" };
-    const [board, setBoard] = useState({writer: '', subject: '', content: '', file: '' });
+    const [board, setBoard] = useState({ subject: '', content: '', writer: '' });
+    const [fileList, setFileList] = useState([]);
+
+    const fileChange = (e) => {
+        if (e.target.files.length > 0) {
+            setFileList([...fileList, e.target.files[0]]);
+        }
+    };
+
+    const changeValue = (e) => {
+        setBoard({ ...board, [e.target.name]: e.target.value })
+    }
 
     const submit = (e) => {
-        e.preventDefault();
-        //서버로부터 accId로 조회해 온 결과를 화면에 출력한다.
-        axios.post(`http://localhost:8090/boardWrite?`)
-            .then(res => {
-                console.log(res)
-                // setAcc({ ...res.data })
-                setBoard({...res.data})
+        const formData = new FormData();
+        formData.append("subject", board.subject);
+        formData.append("content", board.content);
+        formData.append("writer", board.writer);
+        for (let file of fileList){
+            formData.append("file", file);
+        }
+        axios.post(`${url}/boardWrite`, formData)
+            .then(res=>{
+                console.log(res.data);
+            })
+            .catch(err=>{
+                console.log(err)
             })
     }
 
     return (
-        <div>
-            <h5 className="header-text">게시판 글 등록</h5>
+        <>
+            <div className="header-text">게시판 글 등록</div>
             <Form>
                 <FormGroup row>
                     <Label sm={4}>글쓴이</Label>
                     <Col sm={8}>
-                        <Input type="text" id="writer" name="writer" />                        
+                        <Input type="text" id="writer" name="writer" required onChange={changeValue} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={4}>제 목</Label>
                     <Col sm={8}>
-                        <Input type="text" id="subject" name="subject" />                        
+                        <Input type="text" id="subject" name="subject" onChange={changeValue} />
                     </Col>
                 </FormGroup>
                 <FormGroup row >
                     <Label sm={4}>내 용</Label>
                     <Col sm={8}>
-                        <Input type="textarea" id="content" name="content" style={{height:'400px'}} />                        
+                        <Input type="textarea" id="content" name="content" onChange={changeValue} style={{ height: '400px' }} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={4}>이미지 파일 첨부</Label>
                     <Col sm={8}>
-                        <img src='plus.png' style={{height:'100px', justifyItems:'left', display:'flex'}}></img>                        
+                        <img src='plus.png' style={{ height: '100px', justifyItems: 'left', display: 'flex' }} alt='' onClick={() => document.getElementById('file').click()}></img>
+                        <Input type="file" id="file" accept="image/*" hidden onChange={fileChange} />
+                        {
+                            fileList.map((file, index) =>
+                                <span key={index}>
+                                    <img src={URL.createObjectURL(file)} width="100px" alt='' style={{ marginRight: '10px' }} />
+                                    {(index + 1) % 3 == 0 ? <><br /><br /></> : ''}
+                                </span>
+                            )
+                        }
                     </Col>
                 </FormGroup>
-                <Button color='primary' onClick={submit} >등록</Button>
-                <Button>다시쓰기</Button>
+                <div style={{ textAlign: 'center' }}>
+                    <Button color='primary' onClick={submit} >등록</Button>&nbsp;&nbsp;
+                    <Button>다시쓰기</Button>
+                </div>
             </Form>
-        </div>
+        </>
     )
 }
 
